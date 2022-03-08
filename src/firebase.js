96b138aede135de8f038c8e4a4f73f3bb2a1de6b
Fritza02 <fritza.v@hotmail.com>
2022-03-08 15:11:24 -0300

    firebase instalado

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB6Y0rWuybzV_OIrTlBoJFvNW-m7wdH_ZQ",
+    authDomain: "ey-insurance-5a2d0.firebaseapp.com",
+    projectId: "ey-insurance-5a2d0",
+    storageBucket: "ey-insurance-5a2d0.appspot.com",
+    messagingSenderId: "934222267880",
+    appId: "1:934222267880:web:7257870b9bc0eab07fd05d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
