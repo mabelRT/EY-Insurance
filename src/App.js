a086e3f0279662e8ea1afaffd82eb37b043646ca
mabel <wendy.torrico.wt@gmail.com>
2022-03-08 19:07:07 -0300

    componentes creados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
+import Events from './components/Events';
+import Home from './components/Home';
+import Policies from './components/Policies';
 
 const App = () => {
   return (
     <>
         <Routes>
+          <Route path="/events" element={<Events />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/policies" element={<Policies />} />
           <Route path="/" element={<Login />} />
         </Routes>
     </>
